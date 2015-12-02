7d65e1ba92867fe021ee5a194436de3e5d894db2
Gabriel Gayan <ggayan@groupon.com>
2015-12-02 15:19:48 -0800

    Add places

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202185049) do
+ActiveRecord::Schema.define(version: 20151202231852) do
+
+  create_table "places", force: :cascade do |t|
+    t.string   "name"
+    t.string   "full_description"
+    t.string   "geo_location"
+    t.string   "location"
+    t.string   "address"
+    t.string   "image_url"
+    t.string   "coordinates"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "routes", force: :cascade do |t|
     t.string   "title"
