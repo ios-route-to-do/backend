e0014b410dc2c94bbcb58fce2410f78a251cff11
Gabriel Gayan <gabrielgayan@gmail.com>
2015-12-09 15:27:53 -0800

    Fix rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,7 +3,7 @@ class Rating < ActiveRecord::Base
   belongs_to :route
 
   def self.rate(user_id, route_id, rating_score)
-    rating = first_or_initialize(user_id: user_id, route_id: route_id)
+    rating = find_or_initialize_by(user_id: user_id, route_id: route_id)
 
     rating.rating = rating_score
     rating.save!
