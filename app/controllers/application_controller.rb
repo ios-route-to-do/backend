fd2f7ca5c20b5f5c8b81af3b063a2bfee006eaa3
Juan Pablo Marzetti <jmarzetti@groupon.com>
2015-12-08 18:16:34 -0800

    Return favorite routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  before_filter :load_user
+
+  private
+  def load_user
+    @user = User.find(request.headers['X-Jopp-User-Id']) if request.headers['X-Jopp-User-Id']
+  end
+
+  def add_user_metatada_to_route(route)
+    if @user.present?
+      route.favorite = @user.favorite_routes.include?(route)
+    end
+    route
+  end
+
+  def add_user_metadata_to_routes(routes)
+    routes.map { |route| add_user_metatada_to_route(route) }
+  end
+
 end
