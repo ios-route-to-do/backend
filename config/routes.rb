711b4bdb52776a66deae593eb84813f97a05b41e
Gabriel Gayan <gabrielgayan@gmail.com>
2015-12-10 11:03:25 -0800

    Add update user action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope format: false do
-    resources :users, only: [:show] do
+    resources :users, only: [:show, :update] do
       collection do
         post 'login'
         post 'register'
